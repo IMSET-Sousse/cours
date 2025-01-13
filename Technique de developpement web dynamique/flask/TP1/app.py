b71594b255841b1f2cbbf31720c6a3ac172b92fd
Maher-Amara <maheramara32@yahoo.fr>
2025-01-13 13:23:32 +0100

    Update correction TP1

--- Technique de developpement web dynamique/flask/TP1/app.py
+++ Technique de developpement web dynamique/flask/TP1/app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    name = 'Maher'
+    return f'Bonjour {name}'
+
+@app.route('/date')
+def date():
+    return f'La date actuelle est {datetime.now()}'
+
+@app.route('/info')
+def info():
+    name = 'Maher'
+    age = 20
+    return f'Je suis {name} et je suis {age} ans'
 
 if __name__ == '__main__':
     app.run(debug=True)
